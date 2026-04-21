f686268b406086ccd83710f5e2e043ac90f363ee
tharun3421 <20bd1a0596@gmail.com>
2026-04-21 12:17:13 +0530

    changes in server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pdfRoutes from "./routes/pdfRoutes.js";
