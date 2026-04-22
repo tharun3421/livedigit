afc886176ad917a74aa11b53f26a10ca80b45a62
tharun3421 <20bd1a0596@gmail.com>
2026-04-22 12:00:30 +0530

    changed in viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          fontawesome: ["@fortawesome/react-fontawesome", "@fortawesome/free-solid-svg-icons", "@fortawesome/free-brands-svg-icons"],
+          vendor: ["react", "react-dom"],
+        }
+      }
+    }
+  }
 })
